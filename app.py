5be51ae6aa5396a1498d7e993e0709b6ed9954fd
Mark McEwen <mark.mcewen@capita.com>
2020-09-10 16:20:01 +0100

    Refactored: single responsiblity for get busstops

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
-from session_bus_stops import getBusStop1, getBusStop2, getLatLongFromPostcode
+from session_bus_stops import getBusStop, getLatLongFromPostcode
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    r = getBusStop1()
-    r2 = getBusStop2()
+    route1 = '490008660N'
+    route2 = '490008660S'
+    r = getBusStop(route1)
+    r2 = getBusStop(route2)
 
     return render_template('index.html', busStop1 = r, busStop2 = r2)
 
--- session_bus_stops.py
+++ session_bus_stops.py
@@ -6,17 +6,10 @@ def getLatLongFromPostcode():
     pretty_json = json.loads(r.text)
     return json.dumps(pretty_json, indent=2)
 
-def getBusStop1():
-    r = requests.get('https://transportapi.com/v3/uk/bus/stop/490008660N/live.json?app_id=b4a74f33&app_key=7442a17ade5eaf611706d38c1a0b8c4a&group=route&nextbuses=yes')
-    r2 = requests.get('https://transportapi.com/v3/uk/bus/stop/490008660S/live.json?app_id=b4a74f33&app_key=7442a17ade5eaf611706d38c1a0b8c4a&group=route&nextbuses=yes')
-    bus_stop1 = json.loads(r.text)
+def getBusStop(route):
+    r = requests.get(f'https://transportapi.com/v3/uk/bus/stop/{route}/live.json?app_id=b4a74f33&app_key=7442a17ade5eaf611706d38c1a0b8c4a&group=route&nextbuses=yes')
+    bus_stop = json.loads(r.text)
     
-    return bus_stop1['departures'].values()
-
-def getBusStop2():
-    r2 = requests.get('https://transportapi.com/v3/uk/bus/stop/490008660S/live.json?app_id=b4a74f33&app_key=7442a17ade5eaf611706d38c1a0b8c4a&group=route&nextbuses=yes')
-    bus_stop2 = json.loads(r2.text)
-    
-    return bus_stop2['departures'].values()
+    return bus_stop['departures'].values()
 
 #getBusStops()
