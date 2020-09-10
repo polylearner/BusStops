2e2f82ec2c5357a568fb37b75c6582ec0b3e60dd
Mark McEwen <mark.mcewen@capita.com>
2020-09-10 17:08:54 +0100

    Refactored postcode

--- session_bus_stops.py
+++ session_bus_stops.py
@@ -1,10 +1,11 @@
 import requests
 import json
 
-def getLatLongFromPostcode():
-    r = requests.get('https://api.postcodes.io/postcodes/GU50BD')
-    pretty_json = json.loads(r.text)
-    return json.dumps(pretty_json, indent=2)
+def getLatLongFromPostcode(postcode):
+    r = requests.get(f'https://api.postcodes.io/postcodes/{postcode}')
+    postcode = json.loads(r.text)
+    return (postcode['result']['longitude'], postcode['result']['latitude'])
+
 
 def getBusStop(route):
     r = requests.get(f'https://transportapi.com/v3/uk/bus/stop/{route}/live.json?app_id=b4a74f33&app_key=7442a17ade5eaf611706d38c1a0b8c4a&group=route&nextbuses=yes')
@@ -13,3 +14,5 @@ def getBusStop(route):
     return bus_stop['departures'].values()
 
 #getBusStops()
+coord = (getLatLongFromPostcode('nw51tl'))
+#print(getActoCodes(coord))
