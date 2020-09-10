38b3c0f89c930aa6eb4982fcdb037d81d91cfe05
Mark McEwen <mark.mcewen@capita.com>
2020-09-10 15:10:33 +0100

    API work

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from session_bus_stops import getBusStops, getLatLongFromPostcode
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    r = getBusStops()
+
+    return render_template('index.html', busStops = r)
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
--- session_bus_stops.py
+++ session_bus_stops.py
@@ -1,8 +1,14 @@
 import requests
+import json
 
 def getLatLongFromPostcode():
     r = requests.get('https://api.postcodes.io/postcodes/GU50BD')
-    print(r.text)
+    pretty_json = json.loads(r.text)
+    return json.dumps(pretty_json, indent=2)
 
+def getBusStops():
+    r = requests.get('https://transportapi.com/v3/uk/bus/stop/490008660N/live.json?app_id=b4a74f33&app_key=7442a17ade5eaf611706d38c1a0b8c4a&group=route&nextbuses=yes')
+    pretty_json = json.loads(r.text)
+    return json.dumps(pretty_json.departures, indent=2)
 
-getLatLongFromPostcode()
+getBusStops()
